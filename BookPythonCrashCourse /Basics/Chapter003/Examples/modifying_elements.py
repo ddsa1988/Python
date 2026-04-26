57592b87d3facbf62d25a3fa043983718f7e1dd4
Diego Alexandre <ddsa.1988@outlook.com>
2026-04-26 19:54:42 -0300

    commit

---
+++ BookPythonCrashCourse /Basics/Chapter003/Examples/modifying_elements.py
@@ -0,0 +1,20 @@
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+print()
+
+motorcycles[0] = "ducati"
+print(motorcycles)
+print()
+
+motorcycles.append("triumph")
+print(motorcycles)
+print()
+
+motorcycles.insert(0, "royal enfield")
+print(motorcycles)
+print()
+
+motorcycles.remove("triumph")
+del motorcycles[0]
+print(motorcycles)
+print()
