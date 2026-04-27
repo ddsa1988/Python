7fe6b45dad064809fe2ddf8dd5771d128550679d
Diego Alexandre <ddsa1988@outlook.com>
2026-04-27 14:25:43 -0300

    commit

---
+++ BookPythonCrashCourse/Basics/Chapter003/Examples/organizing_list.py
@@ -0,0 +1,25 @@
+cars = ["bmw", "audi", "toyota", 'subaru']
+print(cars)
+
+# The sort method changes the original list
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars)
+print()
+
+# The sorted function doesn't change the original list
+cars = ["bmw", "audi", "toyota", 'subaru']
+cars_sorted = sorted(cars)
+cars_sorted_reverse = sorted(cars, reverse=True)
+print(f"Original list: {", ".join(cars)}")
+print(f"Sorted list: {", ".join(cars_sorted)}")
+print(f"Sorted reverse list: {", ".join(cars_sorted_reverse)}")
+print()
+
+# The reverse function simply reverses the order of the list
+cars = ["bmw", "audi", "toyota", 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
--- BookPythonCrashCourse/Basics/Chapter003/Exercises/shrinking_guest_list.py
+++ BookPythonCrashCourse/Basics/Chapter003/Exercises/shrinking_guest_list.py
@@ -1,5 +1,25 @@
+def cancel_dinner_msg(name: str) -> None:
+    if (type(name) != str):
+        return
+
+    print(f"I'll have to cancel our dinner {name}. I'm sorry.")
+
+
 guest_list = ["Diego", "Amanda", "Amora", "Ameixa",
               "Ivanice", "Tainara", "Rodrigo", "Mireli"]
 
 print(guest_list)
 print()
+
+while (len(guest_list) > 2):
+    popped_name = guest_list.pop()
+    cancel_dinner_msg(popped_name)
+
+print()
+print(guest_list)
+
+while (len(guest_list) > 0):
+    del guest_list[0]
+
+print()
+print(guest_list)
