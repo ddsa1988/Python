4a1643edd919fb230a43a3e8722bc2afe29fff53
Diego Alexandre <ddsa1988@outlook.com>
2026-04-28 07:53:20 -0300

    commit

---
+++ BookPythonCrashCourse/Basics/Chapter003/Exercises/seeing_the_world.py
@@ -0,0 +1,19 @@
+places_to_visit = ["Spain", "Portugal", "China", "Italy", "Africa", "Norway",
+                   "Iceland", "Atacama"]
+
+print(f"Original list: [{", ".join(places_to_visit)}]")
+print()
+
+print(sorted(places_to_visit))
+print()
+
+print(sorted(places_to_visit, reverse=True))
+print()
+
+places_to_visit.sort()
+print(places_to_visit)
+print()
+
+places_to_visit.sort(reverse=True)
+print(places_to_visit)
+print
