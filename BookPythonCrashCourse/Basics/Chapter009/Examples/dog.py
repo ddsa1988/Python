b54fdf9f438aa611888d35b9daa3894dc0fa667b
Diego Alexandre <ddsa.1988@outlook.com>
2026-06-15 21:48:54 -0300

    commit

--- BookPythonCrashCourse/Basics/Chapter009/Examples/dog.py
+++ BookPythonCrashCourse/Basics/Chapter009/Examples/dog.py
@@ -1,30 +1,30 @@
 class Dog:
     """A simple attempt to model a dog"""
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         """Initialize name and sge attributes"""
         self.name: str = name
         self.age: int = age
 
-    def sit(self):
+    def sit(self) -> None:
         """Simulate a dog sitting in response to a command"""
         print(f"{self.name} is now sitting")
 
-    def roll_over(self):
+    def roll_over(self) -> None:
         """Simulate  a dog rolling over in response to a command"""
         print(f"{self.name} rolled over")
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         """Compare if two dog objects are equals"""
         if not isinstance(other, Dog):
-            return NotImplemented
+            return False
 
         return self.name == other.name and self.age == other.age
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Object user-friendly representation"""
         return f"{self.name} is {self.age} years old."
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Object unambiguous representation that mimics the constructor"""
         return f"Dog(name = {self.name}, age = {self.age})"
