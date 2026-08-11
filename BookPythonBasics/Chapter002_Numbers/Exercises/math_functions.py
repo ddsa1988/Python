a1a78dc2a162432493bd5267180b41e0fafad7a1
Diego Alexandre <ddsa1988@outlook.com>
2026-08-11 07:03:12 -0300

    commit

--- BookPythonBasics/Chapter002_Numbers/Exercises/math_functions.py
+++ BookPythonBasics/Chapter002_Numbers/Exercises/math_functions.py
@@ -1,8 +1,8 @@
 def exercise001() -> None:
     decimal_places: int = 2
     number: float
-    prompt: str = "Enter a number: "
 
+    prompt: str = "Enter a number: "
     user_input: str = input(prompt)
 
     try:
@@ -13,19 +13,63 @@ def exercise001() -> None:
 
     result: float = round(number, decimal_places)
 
-    print(f"{number} rounded to {decimal_places} decimal places is {result}")
+    print(f"{number} rounded to {decimal_places} decimal places is {result}.")
 
 
 def exercise002() -> None:
-    pass
+    number: float
+
+    prompt: str = "Enter a negative number: "
+    user_input: str = input(prompt)
+
+    try:
+        number = float(user_input)
+    except:
+        print("Invalid number.")
+        return
+
+    result: float = abs(number)
+
+    print(f"The absolute value of {number} is {result}.")
 
 
 def exercise003() -> None:
-    pass
+    number_1: float
+    number_2: float
+
+    prompt: str = "Enter the first number: "
+    user_input: str = input(prompt)
+
+    try:
+        number_1 = float(user_input)
+    except:
+        print("Invalid number.")
+        return
+
+    prompt = "Enter the second number: "
+    user_input = input(prompt)
+
+    try:
+        number_2 = float(user_input)
+    except:
+        print("Invalid number.")
+        return
+
+    result: bool = (number_1 - number_2).is_integer()
+
+    print(
+        f"The difference between {number_1} and {number_2} is an integer? {result}.")
 
 
 def main() -> None:
     exercise001()
+    print()
+
+    exercise002()
+    print()
+
+    exercise003()
+    print()
 
 
 if (__name__ == "__main__"):
